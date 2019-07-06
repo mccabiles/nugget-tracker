e321a19aa8c045b48b49bfb8d224c19edf4bcacc
Miguel Cabiles <mccabiles@up.edu.ph>
2019-07-06 23:38:43 +0800

    Add tailwind configs

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: [
-    require('tailwindcss')('tailwind.js'),
+    require('tailwindcss')('tailwind.theme.js'),
     require('autoprefixer')(),
   ]
 }
--- tailwind.theme.js
+++ tailwind.theme.js
@@ -1,6 +1,14 @@
 module.exports = {
   theme: {
-    extend: {}
+    colors: {
+      primary: '#5a52a4',
+      secondary: '#af4159',
+      accent: '#664358',
+      dark: '#1e1824',
+      background: '#ded1d6',
+    },
+
+    extend: { }
   },
   variants: {},
   plugins: []
